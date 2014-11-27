d717d736976087e72ebc7191a09fab20f8938ce0
Antonio Valdez Arce <a.valdez.arce@gmail.com>
2014-11-27 23:25:42 +0100

    initial configuration, development stage

--- config/module.config.php
+++ config/module.config.php
@@ -1,7 +1,25 @@
 <?php
+$settings = [
+	'tables' => [
+	    'users' => [
+	       'name' => 'user',
+	       'primary_key_field' => 'user_id',
+	        'fields' => [
+	           'username'      => [ 'type' => 'text', 'required'  => true, ],
+        	   'email'         => [ 'type' => 'email', 'required'  => true, ],
+	           'display_name'  => [ 'type' => 'text', 'required'  => true, ],
+	           'password'      => [ 'type' => 'password', 'required'  => true, ],
+	        ],
+	    ],   
+	],
+    
+    'blocks' => [
+        'users' => [
+            'table' => 'users',
+        ],   
+    ],    
+];
+
 return array(
-	'modules' => array(
-		'ZfcBase',
-		'ZfcUser',
-	),
+    'ava_cms' => $settings,
 );
